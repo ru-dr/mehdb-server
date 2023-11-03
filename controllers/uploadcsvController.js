8a52619175fd7f820d86265ec6d61fb4ec691e99
Lazycoder26 <Rudra26_2004@outlook.com>
2023-11-03 13:42:19 +0530

    Upload csv controller added

--- controllers/uploadcsvController.js
+++ controllers/uploadcsvController.js
@@ -1,26 +1,29 @@
-const LoginData = require("../models/logindata");
 const uploadCsv = require("../models/uploadCsv");
-const { getCurrentTime, generateSrno, getCurrentDate } = require("../utils/utils.js");
+const { generateSrno } = require("../utils/utils.js");
 
 const uploadController = {
   upload: async (req, res) => {
     const { csvData } = req.body;
-    const timeOfLogin = getCurrentTime();
-    const date = getCurrentDate();
-    const srno = generateSrno();
+
     try {
-        console.log("csvData:", csvData);
-        const newCsvEntry = new uploadCsv({
-            csvData
-          });
-      
-          await newCsvEntry.save();
+      if (!csvData) {
+        return res.status(400).json({ message: "CSV data is required" });
+      }
+
+      console.log("done");
+
+      const newCsvEntry = new uploadCsv({
+        csvData,
+      });
+
+      await newCsvEntry.save();
+
+      return res.status(200).json({ message: "Upload successful" });
     } catch (error) {
       console.error("Error during password comparison:", error);
       return res.status(500).json({ message: "Internal server error" });
     }
-
   },
-
 };
+
 module.exports = uploadController;
