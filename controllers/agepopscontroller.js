5f14edba5418c4de1b600ce730cc3990ea28abe2
Vedant Pandey <vedantpandey434@gmail.com>
2023-11-22 16:16:36 +0530

    pops

--- controllers/agepopscontroller.js
+++ controllers/agepopscontroller.js
@@ -4,7 +4,7 @@ const agePopsController = {
     getAgePops: async (req, res) => {
         try {
             const agePops = await AgePopulation.find({});
-            res.json({
+            res.data.json({
                 agePops: agePops,
                 user: req.rootUser,
                 message: "Hello from agepopscontroller.js"
