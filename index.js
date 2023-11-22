fb0c8bbafd3bfb2e14531176bfb3bd31a06cfc0d
Vedant Pandey <vedantpandey434@gmail.com>
2023-11-22 12:12:11 +0530

    kuch bhi

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.use(
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Credentials', 'true');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
   next();
 });
 app.use(cookieParser());
